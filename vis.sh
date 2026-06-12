e8d22ba96dfea6c62f18ed480c967d4cc77a9cc1
lavinal712 <austin712@mail.ustc.edu.cn>
2026-06-12 10:25:12 +0800

    add vis.sh

--- vis.sh
+++ vis.sh
@@ -1,1 +1,4 @@
-python -m vis_mesh --cfg=configs/soke.yaml --demo_dataset=phoenix
+python -m vis_mesh --cfg=configs/signspark/signspark.yaml --demo_dataset=phoenix
+python -m vis_mesh --cfg=configs/signspark/signspark_v2.yaml --demo_dataset=phoenix
+python -m vis_mesh --cfg=configs/signspark/signspark_v3.yaml --demo_dataset=phoenix
+python -m vis_mesh --cfg=configs/signspark/signspark_v4.yaml --demo_dataset=phoenix
